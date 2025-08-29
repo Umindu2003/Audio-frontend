e1eba5af71a8e1460a2967f682b3982764a5283c
Umindu2003 <u4656mee@gmail.com>
2025-08-29 11:15:26 +0530

    init

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import ProductCard from './components/productCard'
+
+function App() {
+  
+
+  return (
+    <>
+      <ProductCard name ="Audio setup" price="LKR 3000/-" description="High-quality audio setup for your home." img="https://plus.unsplash.com/premium_photo-1679913792906-13ccc5c84d44?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8cHJvZHVjdHxlbnwwfHwwfHx8MA%3D%3D" />
+      <ProductCard name ="Smart Phone" price="LKR 50000/-" description="Stay connected and track your fitness with this sleek smartwatch." img="https://images.unsplash.com/photo-1511707171634-5f897ff02aa9?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8c21hcnQlMjB3YXRjaHxlbnwwfHwwfHx8MA%3D%3D" />
+    </>
+  )
+}
+
+export default App
