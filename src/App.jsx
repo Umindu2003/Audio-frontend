2d9be385a22bba8b52833bd8f91c141f44d9ae99
Umindu2003 <u4656mee@gmail.com>
2025-09-01 10:12:20 +0530

    put icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
 import './App.css'
 import ProductCard from './components/productCard'
+import { MdOutlineGraphicEq } from "react-icons/md";
+import { BiBookmarkAlt } from "react-icons/bi";
+import { LuSpeaker } from "react-icons/lu";
+import { FaRegUser } from "react-icons/fa";
 
 function App() {
   return (
-    <div className="bg-red-900">
-      dfafsadfa
+    <div className="w-full h-screen flex">
+      <div className="w-[300px] h-full bg-green-200">
+        <button className="w-full h-[40px] text-[25px] font-bold  flex justify-center items-center"> <MdOutlineGraphicEq /> Dashboard</button>
+        <button className="w-full h-[40px] text-[25px] font-bold  flex justify-center items-center"> <BiBookmarkAlt/> Bookings</button>
+        <button className="w-full h-[40px] text-[25px] font-bold flex justify-center items-center"> <LuSpeaker />Items</button>
+        <button className="w-full h-[40px] text-[25px] font-bold flex justify-center items-center"> <FaRegUser /> Users</button>
+      </div>
+      <div className="w-full bg bg-red-900">
+
+
+      </div>
+      
     </div>
   )
-}
+} 
 
 export default App
