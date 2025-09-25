e0ceab39e72d26b982dc2b4a6134cae193ad78ec
Umindu2003 <u4656mee@gmail.com>
2025-09-25 22:40:33 +0530

    new

--- src/pages/admin/addItemPage.jsx
+++ src/pages/admin/addItemPage.jsx
@@ -1,7 +1,21 @@
 export default function AddItemPage() {
   return (
-    <div className="w-full h-full bg-gray-100">
-      
+    <div className="w-full h-full flex flex-col items-center">
+      <h1>Add New Item</h1>
+      <div className="w-[400px] border flex flex-col items-center"> 
+        <input type="text" placeholder="product key"/>
+        <input type="text" placeholder="product name"/>
+        <input type="text" placeholder="product price"/>
+        <select>
+          <option key="audio">Audio</option>
+          <option key="lights">Lights</option>
+        </select>  
+
+        <input type="text" placeholder="product Dimensions"/>
+        <input type="text" placeholder="product Description"/>
+
+        <button>Add</button>
+      </div>
     </div>
   );
 }
