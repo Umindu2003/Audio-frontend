648f29d0c94329ea90f21335af36c130f6c01ac5
Umindu2003 <u4656mee@gmail.com>
2025-10-24 20:07:07 +0530

    orders approved page and more

--- src/pages/admin/adminPage.jsx
+++ src/pages/admin/adminPage.jsx
@@ -1,3 +1,5 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
 import { MdOutlineGraphicEq } from "react-icons/md";
 import { BiBookmarkAlt } from "react-icons/bi";
 import { LuSpeaker } from "react-icons/lu";
@@ -11,6 +13,32 @@ import AdminOrdersPage from "./adminBookingPage";
 
 
 export default function AdminPage() {
+
+    const [userValidated, setUserValidated] = useState(false);
+  useEffect(()=>{
+    const token = localStorage.getItem("token");
+    if(!token){
+      window.location.href = "/login";
+    }
+    axios.get(`http://localhost:3000/api/users/`,{
+      headers:{
+        Authorization: `Bearer ${token}`
+      }
+    }).then((res)=>{
+      console.log(res.data);
+      const user = res.data;
+      if(user.role == "admin"){
+        setUserValidated(true);        
+      }else{
+        window.location.href = "/";
+      }
+      
+    }).catch((err)=>{
+      console.error(err);
+      setUserValidated(false);
+    })
+  },[])
+
   return (
     <div className="w-full h-screen flex">
       <div className="w-[200px] h-full bg-green-200">
@@ -20,14 +48,14 @@ export default function AdminPage() {
         <Link to ="/admin/users" className="w-full h-[40px] text-[25px] font-bold flex justify-center items-center"> <FaRegUser /> Users</Link>
       </div>
       <div className="w-[calc(100vw-200px)] bg ">
-        <Routes>
-
+        
+        {userValidated&&<Routes>
           <Route path = "/bookings" element={<AdminOrdersPage />} />
           <Route path = "/items" element={<AdminItems />} />
           <Route path = "/users" element={<AdminUsersPage />} />
           <Route path = "/items/add" element={<AddItemPage />} />
           <Route path = "/items/edit" element={<UpdateItemPage />} />
-        </Routes>
+        </Routes>}
 
       </div>
       
